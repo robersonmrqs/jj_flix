7c04030777392b0fe7b231314bead76b4fd07ec1
roberssonmrqs <150697742+robersonmrqs@users.noreply.github.com>
2024-06-01 17:30:33 -0300

    criação e instalação model Video

--- video/models.py
+++ video/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+LISTA_CATEGORIAS = (('JOCA', 'Joca'),
+                    ('JUAN', 'Juan'),
+                    ('EMDUPLA', 'Em Dupla'),
+                    ('ESCOLA', 'Na escola'),
+                    ('EXTRAS', 'Extras'))
+
+class Video(models.Model):
+    titulo = models.CharField(max_length = 50)
+    thumb = models.ImageField(upload_to = 'thumb_videos')
+    descricao = models.TextField(max_length = 200)
+    categoria = models.CharField(max_length = 10, choices = LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default = 0)
+    data_criacao = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.titulo
