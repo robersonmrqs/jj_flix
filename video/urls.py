60378a4a92575dcf14fdaf794dd5e7f27392902f
roberssonmrqs <150697742+robersonmrqs@users.noreply.github.com>
2024-06-01 21:04:18 -0300

    view, url, template detalhesvideo

--- video/urls.py
+++ video/urls.py
@@ -4,14 +4,12 @@ from django.contrib.auth import views as auth_view
 
 app_name = 'video'
 
-urlpatterns = [
-    path('', HomePage.as_view(), name = 'homepage'),
-    path('videos/', HomeVideos.as_view(), name = 'homevideos'),
-    path('videos/<int:pk>', DetalhesVideo.as_view(), name = 'detalhesvideo'),
-    path('pesquisa/', PesquisaVideo.as_view(), name = 'pesquisavideo'),
-    path('login/', auth_view.LoginView.as_view(template_name = 'login.html'), name = 'login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
-    path('editarperfil/<int:pk>', EditarPerfil.as_view(), name = 'editarperfil'),
-    path('criarconta/', CriarConta.as_view(), name = 'criarconta'),
-    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name = 'editarperfil.html', success_url = reverse_lazy('video:homevideos')), name = 'mudarsenha'),
-]
+urlpatterns = [path('', HomePage.as_view(), name = 'homepage'),
+               path('videos/', HomeVideos.as_view(), name = 'homevideos'),
+               path('videos/<int:pk>', DetalhesVideo.as_view(), name = 'detalhesvideo'),
+               path('pesquisa/', PesquisaVideo.as_view(), name = 'pesquisavideo'),
+               path('login/', auth_view.LoginView.as_view(template_name = 'login.html'), name = 'login'),
+               path('logout/', auth_view.LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
+               path('editarperfil/<int:pk>', EditarPerfil.as_view(), name = 'editarperfil'),
+               path('criarconta/', CriarConta.as_view(), name = 'criarconta'),
+               path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name = 'editarperfil.html', success_url = reverse_lazy('video:homevideos')), name = 'mudarsenha')]
--- video/views.py
+++ video/views.py
@@ -4,7 +4,6 @@ from .forms import CriaContaForm, FormHomePage
 from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-
 class HomePage(FormView):
     template_name = 'homepage.html'
     form_class = FormHomePage
@@ -17,13 +16,30 @@ class HomePage(FormView):
 
     def get_success_url(self):
         email = self.request.POST.get('email')
-        usuarios = Usuario.objects.filter(email=email)
+        usuarios = Usuario.objects.filter(email = email)
         if usuarios:
             return reverse('video:login')
         else:
             return reverse('video:criarconta')
 
-
 class HomeVideos(LoginRequiredMixin, ListView):
     template_name = 'homevideos.html'
     model = Video
+
+class DetalhesVideo(LoginRequiredMixin, DetailView):
+    template_name = 'detalhesvideo.html'
+    model = Video
+
+    def get(self, request, *args, **kwargs):
+        video = self.get_object()
+        video.visualizacoes += 1
+        video.save()
+        usuario = request.user
+        usuario.videos_vistos.add(video)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DetalhesVideo, self).get_context_data(**kwargs)
+        videos_relacionados = self.model.objects.filter(categoria = self.get_object().categoria)[0:4]
+        context['videos_relacionados'] = videos_relacionados
+        return context
